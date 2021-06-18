95333246295551a07ebeca91735229bbf8cac1b6
akanshchoudhary79@gmail.com <Akansh1974>
2021-06-18 12:26:17 +0530

    5th

--- main.go
+++ main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/heroku/GoCashager/JsonHelper"
 	"github.com/heroku/GoCashager/utils"
 	_ "github.com/heroku/x/hmetrics/onload"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -37,23 +34,23 @@ func main() {
 
 	router.GET(utils.GET_USER_INFO+"/:uid", func(c *gin.Context) {
 		var uid = c.Param("uid")
-		cursor, err := client_mongo.Database("Cashager").Collection("user+"+uid).Find(ctx, bson.M{"type": "baseInfo"})
-		if err != nil {
-			log.Fatalln(err)
+		// cursor, err := client_mongo.Database("Cashager").Collection("user+"+uid).Find(ctx, bson.M{"type": "baseInfo"})
+		// if err != nil {
+		// 	log.Fatalln(err)
 
-			return
-		}
-		var allItems []bson.M
-		if err = cursor.All(ctx, &allItems); err != nil {
-			log.Fatalln(err)
+		// 	return
+		// }
+		// var allItems []bson.M
+		// if err = cursor.All(ctx, &allItems); err != nil {
+		// 	log.Fatalln(err)
 
-			return
-		}
-		//fmt.Print(allItems[0])
-		var response []byte = JsonHelper.ProvideUserInfo(allItems[0])
-		var res = string(response)
-		fmt.Print(res)
-		c.String(200, res)
+		// 	return
+		// }
+		// //fmt.Print(allItems[0])
+		// var response []byte = JsonHelper.ProvideUserInfo(allItems[0])
+		// var res = string(response)
+		// fmt.Print(res)
+		c.String(200, uid)
 	})
 
 	router.Run(":" + port)
